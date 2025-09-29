549ba8c126865abee344b43c83e71285d25a3560
Framzy <fardenramzy97@gmail.com>
2025-09-29 18:22:02 +0700

    feat: enhance calculator UI with Tailwind CSS styles and restructure button layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,css}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
